620e8085a2ebe0750410f541d1c49ee9150fef9d
Mehmet Akif <mehmetakify180@gmail.com>
2022-10-21 14:59:45 +0300

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,98 @@
+export const tourData = [
+    {
+      id: 1,
+      city: "new york",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM1_jQxVRnosPW7TRYuODt9iPPRBlqK7GyUBFrKvnP3rmqIUiY4aCCrQqgNYap6sulBBo&usqp=CAU" ,
+      name: "new york bridge tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 2,
+      city: "paris",
+      img: "https://www.infojeune.fr/wp-content/uploads/2021/02/culture-louvre.jpg",
+      name: "paris lights tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 3,
+      city: "london",
+      img: "https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTYyNDg1MjE3MTI1Mjc5Mzk4/topic-london-gettyimages-760251843-promo.jpg",
+      name: "london royal palace tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 4,
+      city: "tokyo",
+      img: "https://content.r9cdn.net/rimg/dimg/ca/7e/9ae1c4b2-city-21033-16c1b1c620d.jpg?crop=true&width=1020&height=498",
+      name: "tokyo sushi tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 5,
+      city: "istanbul",
+      img: "https://cdn.goturkiye.com/goturkey/1-safe-in-istanbul-safe-in-turkey.jpg",
+      name: "İstanbul Tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 6,
+      city: "Berlin",
+      img: "https://upload.wikimedia.org/wikipedia/commons/6/6c/Aerial_view_of_Berlin_%2832881394137%29.jpg",
+      name: "Berlin tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 7,
+      city: "Roma",
+      img: "https://italyadaegitim.com/wp-content/uploads/2020/11/kolezyum-roma-1-1-780x470.jpg",
+      name: "Roma tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 8,
+      city: "Amsterdam",
+      img: "https://upload.wikimedia.org/wikipedia/commons/b/be/KeizersgrachtReguliersgrachtAmsterdam.jpg",
+      name: "Amsterdam tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 9,
+      city: "Rio",
+      img: "https://www.altinrota.org/assets/images/uploads/articles/global/2022-05-19-113104-resim1.png",
+      name: "Rio tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 10,
+      city: "Rize",
+      img: "https://img.otelz.com/s3/placeimages/turkey/karadeniz/rize_rize-firtina-vadisi-8d5447.jpg",
+      name: "Rize village tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 11,
+      city: "Helsinki",
+      img: "https://gezilinki.com/wp-content/uploads/2022/01/helsinki-gezi-rehberi.jpeg",
+      name: "Helsinki  tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 12,
+      city: "Köln",
+      img: "https://assets.enuygun.com/media/lib/750x525/uploads/image/5027.jpeg",
+      name: "Köln tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    }
+  ];
